3d90cc22a92f812093abfba7cb0252c2ac87f435
GBLins14 <gabrielglins18@gmail.com>
2026-07-31 23:52:31 -0300

    fix: syncpay api_url

--- src/main/kotlin/com/ndconsultas/bot_whatsapp/whatsapp_gateway/config/SyncPayProperties.kt
+++ src/main/kotlin/com/ndconsultas/bot_whatsapp/whatsapp_gateway/config/SyncPayProperties.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "syncpay")
 data class SyncPayProperties(
-    val apiUrl: String = "https://api.syncpay.com.br",
+    val apiUrl: String = "https://api.syncpayments.com.br/",
     val clientId: String = "",
     val clientSecret: String = "",
     val webhookUrl: String = ""
